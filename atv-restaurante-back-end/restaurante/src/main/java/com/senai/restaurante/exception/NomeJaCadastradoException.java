8ff1a96ff6e0f285f67540a395a5ef0d3f53c3a3
LimaNat <160279258+LimaNat@users.noreply.github.com>
2025-06-09 10:38:24 -0300

    Delete atv-restaurante-back-end/restaurante/src directory

--- atv-restaurante-back-end/restaurante/src/main/java/com/senai/restaurante/exception/NomeJaCadastradoException.java
+++
@@ -1,5 +0,0 @@
-package com.senai.restaurante.exception;
-
-public class NomeJaCadastradoException extends RuntimeException {
-    public NomeJaCadastradoException(String message) {super(message);}
-}
