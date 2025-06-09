a71ca4d913908dd6b62bda253d0378a377a56fd6
LimaNat <160279258+LimaNat@users.noreply.github.com>
2025-06-09 10:47:19 -0300

    Add files via upload

---
+++ atv-restaurante-back-end/restaurante/src/main/java/com/senai/restaurante/exception/NomeJaCadastradoException.java
@@ -0,0 +1,8 @@
+package com.senai.restaurante.exception;
+
+
+public class NomeJaCadastradoException extends RuntimeException {
+    public NomeJaCadastradoException(String message) {super(message);}
+}
+
+
